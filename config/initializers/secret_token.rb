9edc117499d8c396ab765a6454fc5ad119040902
Chase <chase.zollinger@hotmail.com>
2012-07-16 15:10:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HollaGames::Application.config.secret_token = 'f25c614a0b9a518e35793ab8ab87d3c7b43b3bab6154e9a94f264024ec9e7c0f74b56b7e07848fca2034ac59a398f46b30539bcbe7930ec40cefae600ac6a6ee'
